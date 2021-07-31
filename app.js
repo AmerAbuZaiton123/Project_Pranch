b87f0a33c717c387e092bab3c9596dd1a119b4a8
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-07-31 20:24:32 +0300

    Add  Sum And Multiply Function

--- app.js
+++ app.js
@@ -18,10 +18,23 @@ function sum(a, b) { //eslint-disable-line
     let multiply=(a*b)*c;
      return  [ sum1,multiply,`${a} and ${b} and ${c} sum to ${sum1}.` , `The product of ${a} and ${b} and ${c} is ${multiply}.`] ;
    
+  
   }
   
+  
+  function sumArray(sumArr)
+  {  let sum=0
+    for (let i = 0; i < sumArr.length; i++) {
+      sum += sumArr[i];
+  }
+   return[sum,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum} is their sum.`]
+  }
+  
+  let testArray = [2, 3, 4];
+  testSumArray(testArray)
   testSumAndMultiply(4,7,5);
   
   testMultiply(5,9);
   
   testSum(4, 7); 
+  
